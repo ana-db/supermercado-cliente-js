dae69f8ed4e03619e9b3c1af0b2fa2fed8e95e09
Curso <Curso@IPARTEKAULA.COM>
2020-01-27 09:29:08 +0100

    detalle del producto con cards

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 console.log('Empieza script');
 
+const ENDPOINT = 'http://localhost:8080/supermercado-rest/producto/';
+document.getElementById('endpoint').innerHTML = ENDPOINT;
+
 //seleccionar elementos por id:
 let inputEL = document.getElementById('idProducto');
 let botonEL = document.getElementById('boton');
@@ -24,7 +27,7 @@ botonEL.addEventListener("click", () => {
             console.debug("responseText: " + xhr.responseText);
             */
 
-            if(xhr.responseState === 4){ // esperamos a completar la peticion
+            if(xhr.readyState === 4){ // esperamos a completar la peticion
 
                 if(xhr.status === 200){
 
@@ -34,6 +37,10 @@ botonEL.addEventListener("click", () => {
                     //pintamos en texarea
                     //resultadoEL.innerHTML = "id= " + producto.id + " nombre=" + producto.nombre;
                     resultadoEL.innerHTML = `id= ${producto.id} nombre=${producto.nombre}`;
+
+                    document.getElementById('nombre').innerHTML = producto.nombre;
+                    document.getElementById('imagen').src = producto.imagen;
+                    document.getElementById('descripcion').innerHTML = producto.descripcion;
 
                 }
 
@@ -46,7 +53,8 @@ botonEL.addEventListener("click", () => {
           }
            
           //xhr.open('Get', `http://localhost:3000/productos/${inputEL.value}`); //metodo a la uri indicada
-          xhr.open('Get', `http://localhost:8080/supermercado-rest/producto/${inputEL.value}`);
+          //xhr.open('Get', `http://localhost:8080/supermercado-rest/producto/${inputEL.value}`);
+          xhr.open('Get', `${ENDPOINT}${inputEL.value}` );
           xhr.send(); //cuidado es ASINCRONO !!
 
           //resultadoEL.innerHTML = inputEL.value;
